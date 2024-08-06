39bab23ae88ca19cdbc5cc8bfc986a67ae2e14a3
CaioFrancisco <no>
2024-08-06 19:03:44 -0300

    progress??????????

--- common.py
+++ common.py
@@ -8,72 +8,72 @@ from bitarray import bitarray
 aliases for simple/common operations.
 """
 def humanize_floats(to_round):
-	return int(to_round * 100) / 100
+    return int(to_round * 100) / 100
 
 def unpack_int(four_byte_int):
-	return unpack("<i", four_byte_int)[0]
+    return unpack("<i", four_byte_int)[0]
 
 def unpack_char_int(char_int):
-	return unpack("<B", char_int)[0]
+    return unpack("<B", char_int)[0]
 
 def unpack_short_int(short_int):
-	return unpack("<H", short_int)[0]
+    return unpack("<H", short_int)[0]
 
 # ahahaha fuck you 5-bit reads
 def ReadUBitInt(bitarray_to_read, amt_bits):
-	return int(bitarray_to_read.to01(), 2)
+    return int(bitarray_to_read.to01(), 2)
 
 """
 this data structure is very used in demo packet headers.
 the first few bytes determine the length of the data stored.
 the function just reads those bytes, reads the data stored, then returns the data.
 """
-def ReadRawData(file_to_read, data_length_size):
-	data_length = unpack_int(file_to_read.read(data_length_size))
-	return file_to_read.read(data_length)
+def ReadRawDataInt32(file_to_read):
+    data_length = unpack_int(file_to_read.read(4))
+    return file_to_read.read(data_length)
 
-# aliases for common data length sizes
-ReadRawDataInt32 = lambda file_to_read: ReadRawData(file_to_read, 4)
-ReadRawDataInt16 = lambda file_to_read: ReadRawData(file_to_read, 2)
+def ReadRawDataInt16(file_to_read):
+    data_length = unpack_short_int(file_to_read.read(2))
+    return file_to_read.read(data_length)
 
-def ReadAndAllocateString(buf_to_read):
-	MAX_READ = 2048 # max amount of bytes we can read
-	char_counter = 0
-	return_value = ""
+def ReadString(buf_to_read):
+    MAX_READ = 0xFFFF # max amount of bytes we can read
+    char_counter = 0
+    return_value = ""
 
-	while char_counter < MAX_READ:
-		char_counter += 1
-		character = chr(unpack_char_int(buf_to_read.read(1)))
-		return_value += character
-		if character == "\n": return return_value # \n is the string terminator
+    while char_counter < MAX_READ:
+        char_counter += 1
+        character = chr(unpack_char_int(buf_to_read.read(1)))
+        return_value += character
+        if character == "\x00": return return_value # \n is the string terminator
 
-	raise RuntimeError("ReadAndAllocateString over-read. Probably read an invalid chunk of memory.")	
+    raise RuntimeError("String over-read. Probably read an invalid chunk of memory.")	
 
 # just because i'm used to reading stuff like files
 class BitArrayBuffer:
-	bitArray = None
-	index = 0
-	
-	def __init__(self, obj_to_inherit):
-		if type(obj_to_inherit) != type(bitarray()):
-			obj_to_inherit = bitarray(obj_to_inherit, endian="little")
-			
-		self.bitArray = obj_to_inherit
-	
-	def readbit(self, bits_to_read):
-		return_value = bitarray(endian="little")
-		
-		if self.index + bits_to_read > len(self.bitArray): # adjusts for going over-bound
-			bits_to_read -= (self.index + bits_to_read) - len(self.bitArray) 
+    bitArray = None
+    index = 0
+    
+    def __init__(self, obj_to_inherit):
+        if type(obj_to_inherit) != type(bitarray()):
+            obj_to_inherit = bitarray(obj_to_inherit, endian="little")
+            
+        self.bitArray = obj_to_inherit
+    
+    def readbit(self, bits_to_read):
+        return_value = bitarray(endian="little")
+        
+        if self.index + bits_to_read > len(self.bitArray): # adjusts for going over-bound
+            bits_to_read -= (self.index + bits_to_read) - len(self.bitArray) 
 
-		# adds the right chunk of the bitarray into the return value, and adjusts the index accordingly
-		return_value += self.bitArray[self.index : self.index + bits_to_read]
-		self.index += bits_to_read
-			
-		return return_value
-	
-	def read(self, bytes_to_read): # read as bytes
-		return self.read(bytes_to_read * 8).tobytes()
-	
-	def seek(self, bits_to_seek):
-		self.index += bits_to_seek
+        # adds the right chunk of the bitarray into the return value, and adjusts the index accordingly
+        return_value += self.bitArray[self.index : self.index + bits_to_read]
+        self.index += bits_to_read
+            
+        return return_value
+    
+    def read(self, bytes_to_read): # read as bytes
+        return self.readbit(bytes_to_read * 8).tobytes()
+    
+    def seek(self, bits_to_seek):
+        self.index += bits_to_seek
--- main.py
+++ main.py
@@ -6,80 +6,86 @@ from stringtables import *
 from datatables   import *
 
 if len(argv) < 2:
-	print(f"Usage: {argv[0]} [demo name]")
-	exit(1)
-	
+    print(f"Usage: {argv[0]} [demo name]")
+    exit(1)
+    
 class DemoHeader:
-	def __init__(self, file_to_read):
-		file_header = file_to_read.read(8)
-		
-		if file_header != b"HL2DEMO\x00":
-			raise RuntimeError("Invalid .dem file.")
-		
-		self.demo_protocol = unpack_int(file_to_read.read(4))
-		self.net_protocol  = unpack_int(file_to_read.read(4))
-		self.server_name   = file_to_read.read(260).decode("UTF-8")
-		self.client_name   = file_to_read.read(260).decode("UTF-8")
-		self.map_name      = file_to_read.read(260).decode("UTF-8")
-		self.game_dir      = file_to_read.read(260).decode("UTF-8")
-		self.demo_length   = unpack("f", file_to_read.read(4))[0] # measured in seconds
-		self.demo_ticks    = unpack_int(file_to_read.read(4))
-		self.demo_frames   = unpack_int(file_to_read.read(4))
-		self.sign_on_length = unpack_int(file_to_read.read(4))
+    def __init__(self, file_to_read):
+        file_header = file_to_read.read(8)
+        
+        if file_header != b"HL2DEMO\x00":
+            raise RuntimeError("Invalid .dem file.")
+        
+        self.demo_protocol = unpack_int(file_to_read.read(4))
+        self.net_protocol  = unpack_int(file_to_read.read(4))
+        self.server_name   = file_to_read.read(260).decode("UTF-8")
+        self.client_name   = file_to_read.read(260).decode("UTF-8")
+        self.map_name      = file_to_read.read(260).decode("UTF-8")
+        self.game_dir      = file_to_read.read(260).decode("UTF-8")
+        self.demo_length   = unpack("f", file_to_read.read(4))[0] # measured in seconds
+        self.demo_ticks    = unpack_int(file_to_read.read(4))
+        self.demo_frames   = unpack_int(file_to_read.read(4))
+        self.sign_on_length = unpack_int(file_to_read.read(4))
 
 # there should never be a message addressed to 0, so ignore "fuck"
 demo_messages = ["fuck", "dem_signon", "dem_packet", "dem_synctick", "dem_consolecmd",
                  "dem_usercmd", "dem_datatables", "dem_stop", "dem_stringtables", "dem_lastcmd"]
-	
+    
 class DemoPacket:
-	data = []
-	stop = False
-	address = 0x0 # yeah
-	def __init__(self, file_to_read, demoheader):
-		self.address = file_to_read.tell()
-		self.message_type = unpack_char_int(file_to_read.read(1))
-		try:
-			self.message_type = demo_messages[ self.message_type ]
-		except IndexError:
-			print(f"Invalid DemoPacket:\nMessage type: {hex(self.message_type)} - Address: {hex(file_to_read.tell())}")
-			exit(1)
-		self.tick = unpack_int(file_to_read.read(4))
+    data = []
+    stop = False
+    address = 0x0 # yeah
+    def __init__(self, file_to_read, demoheader):
+        self.address = file_to_read.tell()
+        self.message_type = unpack_char_int(file_to_read.read(1))
+        try:
+            self.message_type = demo_messages[ self.message_type ]
+        except IndexError:
+            print(f"Invalid DemoPacket:\nMessage type: {hex(self.message_type)} - Address: {hex(file_to_read.tell())}")
+            exit(1)
+        self.tick = unpack_int(file_to_read.read(4))
 
-		match self.message_type:
-			case "dem_signon":
-				return # TODO: implement parsing dem_signon (do i really need to..?)
-				
-			case "dem_packet":
-				_ = ReadRawDataInt32(file_to_read)
-				return # TODO: also implement parsing this
-				
-			case "dem_synctick":
-				return # since we're just parsing, we can out-right ignore tick syncs.
-				
-			case "dem_consolecmd":
-				self.data.append(ReadRawDataInt32(file_to_read).decode("UTF-8")) # just the command
-			
-			case "dem_usercmd":
-				self.data.append(unpack_int(file_to_read.read(4))) # outgoing sequence, source code documentation says we can discard this
-				self.data.append(file_to_read.read(256).decode("utf-8")) # actual command
-			
-			case "dem_datatables":
-				loaded_datatable = BitArrayBuffer( bitarray(endian="little").from_bytes( ReadRawDataInt32(file_to_read) ) )
-				return # TODO
-				self.data += ParseDatatables(file_to_read)
-				
-			case "dem_stop" | "dem_lastcmd":
-				self.stop = True
-				return # this is the end of the demo!
-			
-			case "dem_stringtables":
-				loaded_stringtables = BitArrayBuffer( bitarray(endian="little").frombytes( ReadRawDataInt32(file_to_read) ) )
-				
-				if len(loaded_stringtables.bitArray) > 5000000 * 8: # 5 megabytes, source treats this as the maximum limit for stringtables
-					raise RuntimeError("Loaded a stringtable above the 5MB limit.")
-				
-				data += ParseStringtables(loaded_stringtables)
-			
+        match self.message_type:
+            case "dem_signon":
+                return # TODO: implement parsing dem_signon (do i really need to..?)
+                
+            case "dem_packet":
+                _ = ReadRawDataInt32(file_to_read)
+                return # TODO: also implement parsing this
+                
+            case "dem_synctick":
+                return # since we're just parsing, we can out-right ignore tick syncs.
+                
+            case "dem_consolecmd":
+                self.data.append(ReadRawDataInt32(file_to_read).decode("UTF-8")) # just the command
+            
+            case "dem_usercmd":
+                self.data.append(unpack_int(file_to_read.read(4))) # outgoing sequence, source code documentation says we can discard this
+                self.data.append(file_to_read.read(256).decode("utf-8")) # actual command
+            
+            case "dem_datatables":
+                loaded_data = bitarray(endian="little")
+                loaded_data.frombytes(ReadRawDataInt32(file_to_read))
+
+                loaded_datatable = BitArrayBuffer( loaded_data )
+                return # TODO
+                self.data += ParseDatatables(file_to_read)
+        
+            case "dem_stop" | "dem_lastcmd":
+                self.stop = True
+                return # this is the end of the demo!
+            
+            case "dem_stringtables":
+                loaded_data = bitarray(endian="little")
+                loaded_data.frombytes(ReadRawDataInt32(file_to_read))
+                
+                loaded_stringtables = BitArrayBuffer( loaded_data )
+                
+                if len(loaded_stringtables.bitArray) > 5000000 * 8: # 5 megabytes, source treats this as the maximum limit for stringtables
+                    raise RuntimeError("Loaded a stringtable above the 5MB limit.")
+                
+                self.data += ParseStringtables(loaded_stringtables)
+            
 demo_file = open(argv[1], "rb")
 curr_demoheader = DemoHeader(demo_file)
 
@@ -88,7 +94,7 @@ header_parsed = f"Game directory: [{curr_demoheader.game_dir}] - Map name: [{cur
                 f"Demo length - In seconds: [{humanize_floats(curr_demoheader.demo_length)}] - In ticks: [{curr_demoheader.demo_ticks}] - In frames: [{curr_demoheader.demo_frames}]\n" + \
                 f"Sign-on data length: {curr_demoheader.sign_on_length}\n" + \
                  "-----------------------\n"
-				
+                
 demo_file.seek(curr_demoheader.sign_on_length, 1)
 
 while True:
